3831d9914931f2b35ea388c6338bda6f406ac179
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2016-08-28 00:02:41 +0200

    Fix sysupgrade(8) copyright.

--- sysinstall/sysupgrade.c
+++ sysinstall/sysupgrade.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016, 2017 Jonas 'Sortie' Termansen.
+ * Copyright (c) 2015, 2016 Jonas 'Sortie' Termansen.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
