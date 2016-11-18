365815401b77770ac611778315cf4666759673f3
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2016-11-18 18:39:55 +0100

    Add openpty(3).

---
+++ libc/pty/openpty.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Jonas 'Sortie' Termansen.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * termios/openpty.c
+ * Create and open a pseudoterminal.
+ */
+
+#include <sys/ioctl.h>
+
+#include <pty.h>
+#include <string.h>
+#include <termios.h>
+
+int openpty(int* masterfd,
+            int* slavefd,
+            char* name,
+            const struct termios* tio,
+            const struct winsize* ws)
+{
+	if ( mkpty(masterfd, slavefd, 0) < 0 )
+		return -1;
+	if ( name )
+	{
+		memcpy(name, "/dev/", 5);
+		ioctl(*masterfd, TIOCGNAME, name + 5);
+	}
+	if ( tio )
+		tcsetattr(*slavefd, TCSANOW, tio);
+	if ( ws )
+		ioctl(*masterfd, TIOCSWINSZ, ws);
+	return 0;
+}
